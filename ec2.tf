60f7654f1c33812bdb54826560c85acf12c1d0c6
Jordan <jcooke716@gmail.com>
2024-02-01 15:10:46 -0500

    update

--- ec2.tf
+++ ec2.tf
@@ -8,7 +8,7 @@ provider "aws" {
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket  = "aosnote-terraform-state-bucket"
+    bucket  = "jcooke716-terraform-remote-state"
     key     = "build/terraform.tfstate"
     region  = "us-east-1"
     profile = "terraform-user"
